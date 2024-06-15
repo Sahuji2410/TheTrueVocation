7f9fb301b46948a076f678cb0eac4ed52f6ea71b
Sanskar Sahu <sahusanskar78@gmail.com>
2024-06-15 20:30:44 +0530

    added vercel file

--- CareersBucket/settings.py
+++ CareersBucket/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-k*!b2@p%8fkye#-l9_bd^v)2x1tq5s6i6q1ymq*vfa0(_z14wu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
